31528e9200ddb1f578311c15b862ac5887fc6941
evangallagher <evga7768@colorado.edu>
2019-09-26 22:40:02 -0600

    commit of working get_data.py and math_lib.py

--- get_data.py
+++ get_data.py
@@ -1,3 +1,22 @@
+import sys
+
 
 def read_stdin_col(col_num):
-    return None
+    """ This function returns an array of the stdin column
+
+    Parameters
+    -----------
+    col_num : the column of data
+
+    Returns
+    --------
+    L : a list containing the specified column
+    """
+
+    L = []
+
+    # stdin may come from gen_data.sh
+    for l in sys.stdin:
+        A = l.rstrip().split()
+        L.append(float(A[col_num]))
+    return L
--- math_lib.py
+++ math_lib.py
@@ -1,6 +1,38 @@
+import math
+import sys
+
 
 def list_mean(L):
-    return None
+
+    if L is None:
+        return None
+
+    if len(L) == 0:
+        return None
+
+    try:
+        mean = sum(L)/len(L)
+
+    except TypeError:
+        raise TypeError('Unexpected Value')
+
+    return mean
+
 
 def list_stdev(L):
-    return None
+
+    if L is None:
+        return None
+
+    if len(L) == 0:
+        return None
+
+    try:
+        s = sum(L)/len(L)
+
+    except TypeError:
+        raise TypeError('Unexpected value')
+
+    stdv = math.sqrt(sum([(s - x) ** 2 for x in L]) / (len(L) - 1))
+
+    return stdv
